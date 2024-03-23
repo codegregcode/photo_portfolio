a55df88e7e48836119141bc28966b7bf6d2919a8
Greg Cain <codegregcode@gmail.com>
2024-03-23 23:04:10 +0000

    updates Title test and App snapshot

--- src/__tests__/Title.test.js
+++ src/__tests__/Title.test.js
@@ -1,15 +1,30 @@
-import React, { render, screen } from '@testing-library/react';
+import React, { render, screen, fireEvent } from '@testing-library/react';
 import Title from '../components/Title';
 
 describe('Title', () => {
-  it('renders title h1', () => {
+  beforeEach(() => {
     render(<Title />);
+  });
+
+  it('renders title button', () => {
+    const buttonElement = screen.getByText(/enter/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  it('renders title h1 after button click', () => {
+    const buttonElement = screen.getByText(/enter/i);
+
+    fireEvent.click(buttonElement);
+
     const h1Element = screen.getByText(/echium_photo/i);
     expect(h1Element).toBeInTheDocument();
   });
 
-  it('render title img', () => {
-    render(<Title />);
+  it('render title img after button click', () => {
+    const buttonElement = screen.getByText(/enter/i);
+
+    fireEvent.click(buttonElement);
+
     const imgElement = screen.getByAltText(/Title Background/i);
     expect(imgElement).toBeInTheDocument();
   });
