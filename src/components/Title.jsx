13cbae47385e5a5b677aa39b273688a799e90920
Greg Cain <codegregcode@gmail.com>
2024-03-23 23:04:33 +0000

    updates Title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import gifBackground from '../assets/animate.gif';
 import '../styles/title.css';
 
 const Title = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [showEnterText, setShowEnterText] = useState(true);
+
+  const handleEnterClick = () => {
+    setShowEnterText(false);
+    setIsVisible(true);
+  };
+
   return (
-    <div className="title-container">
-      <h1 className="title-text">echium_photo</h1>
-      <img
-        src={gifBackground}
-        className="title-background"
-        alt="Title Background"
-      />
+    <div className="title-component">
+      {showEnterText && (
+        <button type="button" onClick={handleEnterClick}>
+          enter
+          <br />
+          (warning flashing images)
+        </button>
+      )}
+      {isVisible && (
+        <div className="title-container">
+          <h1 className="title-text">echium_photo</h1>
+          <img
+            src={gifBackground}
+            className="title-background"
+            alt="Title Background"
+          />
+        </div>
+      )}
     </div>
   );
 };
