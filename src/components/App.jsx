6b0ad782a133abaf674cab6f8dd045770ad89895
Greg Cain <codegregcode@gmail.com>
2024-03-23 19:06:29 +0000

    imports Title into App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Title from './Title';
 import '../styles/app.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <Routes>
+        <Route path="/" element={<Title />} />
+      </Routes>
     </div>
   );
 };
