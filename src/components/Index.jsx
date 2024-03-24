0a2a463e1019da0be20fc2d476188eae43fb2e66
Greg Cain <codegregcode@gmail.com>
2024-03-24 00:02:14 +0000

    creates Index

---
+++ src/components/Index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Index = () => {
+  return (
+    <div className="index-component">
+      <div className="index-text">
+        <p>a photographer based in Leeds</p>
+        <p>mainly shooting film</p>
+        <p>see what i&apos;ve been up to:</p>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
