1a6c324be3a844614ec2edc71c5287ebc0fa3801
Greg Cain <codegregcode@gmail.com>
2024-03-25 21:19:43 +0000

    updates test

--- src/__tests__/Index.test.js
+++ src/__tests__/Index.test.js
@@ -7,9 +7,11 @@ describe('Index', () => {
     const pOne = screen.getByText(/a photographer based in Leeds/i);
     const pTwo = screen.getByText(/mainly shooting film/i);
     const pThree = screen.getByText(/see what i've been up to:/i);
+    const pFour = screen.getByText(/follow me on instagram/i);
 
     expect(pOne).toBeInTheDocument();
     expect(pTwo).toBeInTheDocument();
     expect(pThree).toBeInTheDocument();
+    expect(pFour).toBeInTheDocument();
   });
 });
