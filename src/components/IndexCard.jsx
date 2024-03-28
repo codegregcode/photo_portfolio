85667666f81764dee7589c5d5a64df6787ee2767
Greg Cain <codegregcode@gmail.com>
2024-03-28 11:14:52 +0000

    adds BrowserRouter as Router for IndexCard render

--- src/components/IndexCard.jsx
+++ src/components/IndexCard.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { animated } from 'react-spring';
 import getJournals from '../firebase';
 import '../styles/index-card.css';
@@ -35,21 +36,23 @@ const IndexCard = () => {
   return (
     <div className="index-card-component">
       {journals.map((journal, index) => (
-        <animated.div
-          key={journal.name}
-          className="index-card"
-          title={`take me to ${journal.name}`}
-          style={{
-            opacity: isVisible[index] ? 1 : 0,
-            transform: isVisible[index]
-              ? 'translateY(0px)'
-              : 'translateY(600px)',
-            transition: 'opacity 0.5s, transform 0.5s',
-          }}
-        >
-          <img src={journal.thumb} alt={`${journal.name} thumb`} />
-          <h5>{journal.name}</h5>
-        </animated.div>
+        <Link to={journal.path}>
+          <animated.div
+            key={journal.name}
+            className={`index-card ${index % 2 !== 0 ? 'odd' : 'even'}`}
+            title={`take me to ${journal.name}`}
+            style={{
+              opacity: isVisible[index] ? 1 : 0,
+              transform: isVisible[index]
+                ? 'translateY(0px)'
+                : 'translateY(600px)',
+              transition: 'opacity 0.5s, transform 0.5s',
+            }}
+          >
+            <img src={journal.thumb} alt={`${journal.name} thumb`} />
+            <h5>{journal.name}</h5>
+          </animated.div>
+        </Link>
       ))}
     </div>
   );
