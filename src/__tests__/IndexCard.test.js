35fcd70a132c5c9de3467b918aaa886b004efb7c
Greg Cain <codegregcode@gmail.com>
2024-05-27 18:40:53 +0100

    updates IndexCard test and updates App snapshot

--- src/__tests__/IndexCard.test.js
+++ src/__tests__/IndexCard.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { render, waitFor } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import IndexCard from '../components/IndexCard';
+
 import getJournals from '../firebase';
 
 jest.mock('../firebase');
+jest.mock('react-toastify', () => ({
+  toast: {
+    error: jest.fn(),
+  },
+}));
 
 describe('IndexCard component', () => {
   it('displays journals after fetching data', async () => {
@@ -34,6 +41,21 @@ describe('IndexCard component', () => {
       expect(getByText('Journal 2')).toBeInTheDocument();
       expect(getByAltText('Journal 1 thumb')).toBeInTheDocument();
       expect(getByAltText('Journal 2 thumb')).toBeInTheDocument();
+    });
+  });
+
+  it('displays toast error when fetching data fails', async () => {
+    const errorMessage = 'Failed to fetch journals';
+    getJournals.mockRejectedValueOnce(new Error(errorMessage));
+
+    render(
+      <Router>
+        <IndexCard />
+      </Router>
+    );
+
+    await waitFor(() => {
+      expect(toast.error).toHaveBeenCalledWith(errorMessage);
     });
   });
 });
