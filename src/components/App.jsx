9e1e68806792e3f30c686bcb7179f439168890d4
Greg Cain <codegregcode@gmail.com>
2024-05-27 18:39:49 +0100

    installs toast and imports into App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
 import Title from './Title';
 import Index from './Index';
+
 import Mar172024 from './journals/Mar172024';
 import Apr272024 from './journals/Apr272024';
+
 import '../styles/app.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div className="app-container">
+      <ToastContainer />
       <Routes>
         <Route path="/" element={<Title />} />
         <Route path="/index" element={<Index />} />
