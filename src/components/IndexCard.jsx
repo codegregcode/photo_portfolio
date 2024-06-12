d89995443f48b263327085d3b8bc517e2de9e3fa
Greg Cain <codegregcode@gmail.com>
2024-06-12 20:05:30 +0100

    progress on IndexCard

--- src/components/IndexCard.jsx
+++ src/components/IndexCard.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { animated } from 'react-spring';
 import { toast } from 'react-toastify';
+import useAnimation from '../hooks/useAnimation';
 
 import getJournals from '../firebase';
 import '../styles/index-card.css';
@@ -9,6 +10,12 @@ import '../styles/index-card.css';
 const IndexCard = () => {
   const [isVisible, setIsVisible] = useState(false);
   const [journals, setJournals] = useState([]);
+  const showAnimation =
+    sessionStorage.getItem('show_journal_animation') !== '1';
+
+  console.log(
+    `IndexCard-showAnimation: ${showAnimation} -useAnimation: ${useAnimation()}`
+  );
 
   useEffect(() => {
     const fetchJournals = async () => {
@@ -24,16 +31,38 @@ const IndexCard = () => {
     fetchJournals();
   }, []);
 
+  // perhaps set a new marker in sessionStorage ie indexCard showAnimation that if is 1 does not show animation if doesn't exist it plays??
   useEffect(() => {
     if (isVisible && journals.length > 0) {
+      let totalDelay = 0;
+
       journals.forEach((j, index) => {
         setTimeout(
-          () => setIsVisible((prev) => ({ ...prev, [index]: true })),
-          4800 + index * 200
+          () => {
+            setIsVisible((prev) => ({ ...prev, [index]: true }));
+          },
+          !showAnimation ? 0 : 4800 + index * 200
         );
+
+        totalDelay = !showAnimation ? 0 : 4800 + (index + 1) * 200;
       });
+
+      setTimeout(() => {
+        console.log('The end of the array');
+        sessionStorage.setItem('show_journal_animation', '1');
+      }, totalDelay);
     }
   }, [isVisible, journals]);
+
+  useEffect(() => {
+    const handleUnload = () => {
+      sessionStorage.removeItem('show_journal_animation');
+    };
+    window.removeEventListener('beforeunload', handleUnload);
+    return () => {
+      window.removeEventListener('beforeunload', handleUnload);
+    };
+  }, []);
 
   return (
     <div className="index-card-component">
